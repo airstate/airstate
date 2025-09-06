9c3b4ef9e11418f5b4eccbb615414ce9382726c4
Omran Jamal <o.jamal97@gmail.com>
2025-09-06 22:32:27 +0600

    changed vite server settings

--- example/light-switch/vite.config.ts
+++ example/light-switch/vite.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
-  server: {
-    host: "0.0.0.0"
-  }
+    plugins: [tailwindcss(), react()],
+    server: {
+        host: '0.0.0.0',
+        port: 19002,
+        allowedHosts: true,
+    },
 });
